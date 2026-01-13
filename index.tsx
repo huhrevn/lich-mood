a2ce4ec258a786abe24c65792216f61106de4207
Đinh Hân <huhrevn@gmail.com>
2026-01-13 20:26:46 +0700

    Hoan tat tinh nang Lich Google

--- index.tsx
+++ index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import UserAuth from './UserAuth'; // <-- 1. Import cái nút vừa làm
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -11,8 +9,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {/* 2. Đặt nút đăng nhập vào đây, nó sẽ nằm đè lên trên App */}
-    <UserAuth />
     <App />
   </React.StrictMode>
 );
